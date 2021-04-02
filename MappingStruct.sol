85cd2386ed60490ee9889341337ed418739d1a71
dongquan0393 <81600625+dongquan0393@users.noreply.github.com>
2021-04-02 16:49:17 +0700

    Add files via upload

---
+++ MappingStruct.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.5.13;
+
+contract MappingStruct {
+    
+    
+    struct Payment {
+        uint amount ;
+        uint timestamp;
+    }
+    
+    struct Balance {
+        uint totalBalance;
+        uint numPayments;
+        mapping (uint => Payment) Payments;
+    }
+    
+    
+    mapping (address => Balance) public balanceReceived;
+    
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+    
+    function sendMoney() public payable {
+        balanceReceived[msg.sender].totalBalance += msg.value;
+        
+        Payment memory payment = Payment(msg.value, now);
+        balanceReceived[msg.sender].Payments[balanceReceived[msg.sender].numPayments] = payment;
+        balanceReceived[msg.sender].numPayments ++;
+        
+    }
+    
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require (balanceReceived[msg.sender].totalBalance >= _amount, "Not enough funds");
+        balanceReceived[msg.sender].totalBalance -= _amount;
+        _to.transfer(_amount);
+    }
+    
+    
+    function withdrawAllMoney(address payable _to) public {
+        
+        uint balanceTosend = balanceReceived[msg.sender].totalBalance;
+        balanceReceived[msg.sender].totalBalance=0;
+        _to.transfer(balanceTosend);
+        
+    }
+}
